308b0262e6a03814071d03498a220b6ee5765bde
Alex McDaniel <amcdaniel186@gmail.com>
2018-10-31 18:40:45 -0400

    update

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -1,4 +1,5 @@
-var cards = [
+let cards = [
+
   { name: 'aquaman',         img: 'aquaman.jpg' },
   { name: 'batman',          img: 'batman.jpg' },
   { name: 'captain america', img: 'captain-america.jpg' },
@@ -23,12 +24,18 @@ var cards = [
   { name: 'superman',        img: 'superman.jpg' },
   { name: 'the avengers',    img: 'the-avengers.jpg' },
   { name: 'thor',            img: 'thor.jpg' }
+
 ];
 
 $(document).ready(function(){
-  var memoryGame = new MemoryGame(cards);
-  var html = '';
+
+  let memoryGame = new MemoryGame(cards);
+  let html = '';
+
+  memoryGame.shuffleCards(cards);
   memoryGame.cards.forEach(function (pic) {
+
+    
     html += '<div class="card" data-card-name="'+ pic.name +'">';
     html += '  <div class="back" name="'+ pic.img +'"></div>';
     html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
@@ -41,7 +48,14 @@ $(document).ready(function(){
   // Bind the click event of each element to a function
   $('.back').click(function () {
     // TODO: write some code here
+$(this).siblings().addClass('back')
+$(this).hide()
   });
 });
 
+
+// structure: each div is class card. in that div, there r 2 divs, like a playing card. trying to flip it, delete the back of the card, last card goes away, gets moved. inspect document to find disappeared div. find hidden div and adjust width to make it bigger. when back is clicked, card needs to switch to the front with image. check how to ask if a div's class matches the div class of the clicked div. adjust time - setTimeOut
+// setTimeOut(()=>{
+
+// memorygame constructor
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,117 @@
-var MemoryGame = function (cards) {
-  this.cards = cards;
-};
-
-MemoryGame.prototype.shuffleCards = function () {
-};
-
-MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+class MemoryGame {
+  constructor(cards){
+    this.cards = cards;
+    this.pickCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
 }
 
-MemoryGame.prototype.isFinished = function () {
-};
+  shuffleCards(cards) {
+    let arrPush = [];
+  for(let i = this.cards.length; i > 0; i--){
+    let randomCardIndex = Math.floor(Math.random()*(i));
+    arrPush.push(this.cards[randomCardIndex]);
+  
+    this.cards.splice(randomCardIndex,1);
+   
+    this.cards = arrPush;
+    return arrPush;
+  };
+  
+
+
+
+
+  // if (game.checkIfPair = this.pairsClicked
+  
+memoryGame.checkIfPair = function (firstCard, secondCard){
+      }
+  
+memoryGame.isFinished = function () {
+      };
+    };
+}
+// class is the blueprint of the objects. ex: class objectConstructor
+// create a constructor function, with characteristics that all the objects will have.
+// tell blueprint, I want to create constructor function o fobjects tha twill take a property that'll have color and the other property will be console.log (x).
+
+// class objectConstructor {
+//  constructor(color){
+//    this.color = color
+//   }
+//   run(x){
+//     console.log(x)
+//   }
+//   let obj1 = new objectConstructor('yellow')
+
+//   let myNewVar = new objectConstructor('taco')
+//   myNewVar.run('yum')
+
+
+
+
+// vvvv below, Isaiah's shuffle code
+// class MemoryGame{
+//   constructor(cards){
+//     this.cards = cards;
+//     this.pickCards = [];
+//     this.pairsClicked = 0;
+//     this.pairsGuessed = 0;
+//   }
+
+
+
+//   shuffleCards(cards) {
+//     let arrPush = [];
+//     for(let i = this.cards.length; i > 0; i--){
+//     let randomCardIndex = Math.floor(Math.random()*(i));
+//     arrPush.push(this.cards[randomCardIndex]);
+    
+//     this.cards.splice(randomCardIndex,1);  
+//     }
+//     console.log("============ ", arrPush);
+//     console.log("--------------- ", cards)
+//   this.cards = arrPush;
+//   return arrPush;
+//   };
+
+// }
+// ^^^^^^^^^^^^^^^^
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // let cards = ['','']
+  // let starwars = ['','']
+  // let pokemon = ['','']
+  // class memoryGame {
+  //   constructor(cards){
+  //     this.cards = shuffleCards
+  //     this.emptyArr = emptyArr
+  //     this.counter = 0
+  //   }
+   
+  //     }
+  //     pickCards(card){
+  //       this.emptyArr.push(this.click.name)
+  //       if(emptyArr[0]===emptyArr[1]){
+  //         counter ++
+  //       emptyArr=[]
+  //       })
+
+  //     }
+  // }
+
+  // let myGame = new gameBoard(cards)
+
+  // let rachelGame = new gameBoard(starwars)
